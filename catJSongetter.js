437f57fc9e834d1648d61e865aeb6da41b3f9d2a
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-12-12 16:49:00 -0500

    Print 'Maine Coon' using async await and .then syntax for comparison Co-authored-by: Paul Antonio <Pantonio2000@gmail.com>

---
+++ catJSongetter.js
@@ -0,0 +1,32 @@
+// importing fetch
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+// Goal: Print the cat breed name to console.
+
+// with Async Await
+
+// const catJSONGetter = async () => {
+//   try {
+//     const cat = await fetch(
+//       "https://api.thecatapi.com/v1/breeds/search?q=maine%20coon"
+//     );
+//     const catJSON = await cat.json();
+//     console.log(catJSON[0].description);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// With .then syntax
+const catJSONGetter = () => {
+  try {
+    return fetch("https://api.thecatapi.com/v1/breeds/search?q=maine%20coon")
+      .then((cat) => cat.json())
+      .then((catJSON) => console.log(catJSON[0].name));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+catJSONGetter(); // => Main Coon
